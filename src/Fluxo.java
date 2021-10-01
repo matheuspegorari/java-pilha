29b610ab98bc3654243a35ef20931a08538f2712
matheuspegorari <pegorari42@gmail.com>
2021-10-01 18:05:15 -0300

    Exceptions study

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -3,12 +3,19 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
+        try {
         metodo1();
+        } catch(ArithmeticException | NullPointerException ex) {
+        	String msg = ex.getMessage();
+        	System.out.println("Exception " + msg);
+        	ex.printStackTrace();
+        }
         System.out.println("Fim do main");
     }
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
+        
         metodo2();
         System.out.println("Fim do metodo1");
     }
@@ -17,7 +24,10 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            Conta c = null;
+            c.deposita();
         }
+        
         System.out.println("Fim do metodo2");
     }
 }
